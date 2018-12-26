607b16339dfdde0d3ca5cd19cdc78751bb1811f6
Maxim <mekhokhlov_1@edu.hse.ru>
2018-12-26 17:01:39 +0300

    Created interface (2/2). Added language change.

--- BackUpMaster/MainWindow.xaml.cs
+++ BackUpMaster/MainWindow.xaml.cs
@@ -24,5 +24,142 @@ namespace BackUpMaster
         {
             InitializeComponent();
         }
+
+        //
+        // Event Handlers.
+        //
+        
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            InitUILables();
+        }
+
+        private void FolderChooseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnglishButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_language != UILanguage.English)
+            {
+                _language = UILanguage.English;
+                UpdateLabels();
+            }
+        }
+
+        private void RussianButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_language != UILanguage.Russian)
+            {
+                _language = UILanguage.Russian;
+                UpdateLabels();
+            }
+        }
+
+        private void OptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        //
+        // Custom private methods
+        //
+
+        private void UpdateLabels()
+        {
+            foreach(KeyValuePair<Label, UILabelPair> pair in _UILabels)
+            {
+                pair.Key.Content = pair.Value.GetUILabel(_language);
+            }
+
+            foreach(KeyValuePair<Button, UILabelPair> pair in _UIButtonLabels)
+            {
+                pair.Key.Content = pair.Value.GetUILabel(_language);
+            }
+
+            foreach(KeyValuePair<Label, UILabelPair> pair in _UIDataLabels)
+            {
+                string temp = String.Empty;
+                if (_language == UILanguage.English)
+                    temp = pair.Value.GetUILabel(UILanguage.Russian);
+                else
+                    temp = pair.Value.GetUILabel(UILanguage.English);
+
+                if (pair.Key.Content.Equals(temp))
+                    pair.Key.Content = pair.Value.GetUILabel(_language);
+            }
+        }
+
+        private void InitUILables()
+        {
+            _UILabels = new Dictionary<Label, UILabelPair>()
+            {
+                { DiskUILabel, new UILabelPair("Choose your disk to back-up:", "Выберите диск для резервного копирования:") },
+                { DateUILabel, new UILabelPair("Choose Date:", "Выберите дату:") },
+                { ModeUILabel, new UILabelPair("Choose what files to back-up:","Выберите какие файлы копировать:") },
+                { FolderUILabel, new UILabelPair("Choose the back-up folder:","Выберите папку для сохранения:") },
+                { SpaceLabel, new UILabelPair("Space needed: ","Необходимо места: ") },
+                { StorageLabel, new UILabelPair("Free storage space: ","Свободная память: ") },
+                { FilesNumbLabel, new UILabelPair("Files to back-up: ","Файлов на сохранение: ") },
+            };
+
+            _UIDataLabels = new Dictionary<Label, UILabelPair>()
+            {
+                { FolderDisplayLabel, new UILabelPair("No folder chosen.","Папка не выбрана.") },
+                { SpaceDigitLabel, new UILabelPair("none","пусто") },
+                { StorageDigitLabel, new UILabelPair("none","пусто") },
+                { FilesNumbsDigitLabel, new UILabelPair("none","пусто") }
+            };
+
+            _UIButtonLabels = new Dictionary<Button, UILabelPair>()
+            {
+                { FolderChooseButton, new UILabelPair("Choose","Выбрать") },
+                { StartButton, new UILabelPair("Start Back-Up","Начать сохранение") },
+                { OptionsButton, new UILabelPair("Options","Опции") }
+            };
+        }
+
+        //
+        // Data
+        //
+
+        private static UILanguage _language = UILanguage.English;
+        private Dictionary<Label, UILabelPair> _UILabels;
+        private Dictionary<Label, UILabelPair> _UIDataLabels;
+        private Dictionary<Button, UILabelPair> _UIButtonLabels;
+    }
+
+    public enum UILanguage { Russian, English }
+
+    internal class UILabelPair
+    {
+        string Russian;
+        string English;
+        
+
+        public string GetUILabel(UILanguage currentLanguage)
+        {
+            if (currentLanguage == UILanguage.Russian)
+                return Russian;
+            else
+                return English;
+        }
+
+        public UILabelPair(string eng, string rus)
+        {
+            Russian = rus;
+            English = eng;
+        }
     }
 }
